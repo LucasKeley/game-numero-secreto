5214477fc61f7a105fa5cfde4aebcf00b58888db
Lucas Keley <44778726+LucasKeley@users.noreply.github.com>
2024-11-04 23:40:19 -0300

    adição de variaveis iniciais

---
+++ app.js
@@ -0,0 +1,7 @@
+alert("Boas vindas ao jogo do número secreto");
+let numeroSecreto = 5;
+let chute = prompt("Escolha um número entre 1 um é 10");
+
+if(numeroSecreto == chute){
+    console.log("Você descobriu o número secreto é", chute)
+};
