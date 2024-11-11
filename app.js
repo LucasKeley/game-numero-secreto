0f7834a0c88b09caa18625262a4ab213e5710c7f
lucaskeley <lucaskeley01@gmail.com>
2024-11-10 22:56:48 -0300

    Implementação da geração aleatória do número secreto e condições para verificar acerto ou erro

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 alert("Boas vindas ao jogo do número secreto");
-let numeroSecreto = 5;
-let chute = prompt("Escolha um número entre 1 um é 10");
 
-if(numeroSecreto == chute){
-    console.log("Você descobriu o número secreto é", chute)
+let numeroMaximo = 100;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+let chute;
+let tentativas = 0;
+
+while(numeroSecreto != chute){
+    chute = prompt(`Escolha um número entre 1 um e ${numeroMaximo}`);
+    tentativas++;
+    if(numeroSecreto == chute){
+        break;
+    } else{
+        if(numeroSecreto > chute){
+            alert(`O numero secreto é maior que ${chute}`);
+        } else{
+            alert(`O número secreto é menor que ${chute}`);
+        }
+    }
 };
+
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(`Você descobriu o número secreto é ${numeroSecreto} você acertou com ${tentativas} ${palavraTentativa}`);
